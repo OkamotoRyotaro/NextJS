911931c9d45a4b4ccde04785bf382965d36737b0
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-04-11 14:29:55 +0900

    add hero page

--- website/src/app/top/page.tsx
+++ website/src/app/top/page.tsx
@@ -1,5 +1,10 @@
 import fs from "fs";
+import Image from "next/image";
 import path from "path";
+import About from "../about/page";
+import Event from "../event/page";
+import Contact from "../contact/page";
+import style from "@/app/style/top.module.css"
 
 const Top = async () => {
   // text.txtのパスを取得
@@ -7,13 +12,31 @@ const Top = async () => {
   const fileContents = fs.readFileSync(filePath, "utf8");
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Top Page</h1>
-      <p className="mt-4">Welcome to the Top page!</p>
-      <div className="mt-8 p-4 border rounded bg-gray-100">
+    <div>
+      <div className={style.heroArea}>
+        {/* 背景画像 */}
+        <Image
+          src={"globe.svg"}
+          alt="background"
+          width={100}
+          height={100}
+          className={style.heroImage}
+        />
+        {/* 画像上の文字 */}
+        <div className={style.heroText}>
+          <h2 className={style.h2}>
+            Overlay Text
+          </h2>
+        </div>
+      </div>
+
+      <div className="mt-8 p-4 border rounded">
         <h2 className="text-xl font-semibold">File Content:</h2>
         <pre className="mt-2 whitespace-pre-wrap">{fileContents}</pre>
       </div>
+      <Event />
+      <About />
+      <Contact />
     </div>
   );
 };
