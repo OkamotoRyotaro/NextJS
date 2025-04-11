bbe928b7a0116598e34f11245ef9de57e158c63a
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-04-11 13:48:28 +0900

    メンバーにid属性を追加して、繰り返しの key に設定

--- website/public/menbers/MenberStatus.ts
+++ website/public/menbers/MenberStatus.ts
@@ -1,4 +1,5 @@
 export interface MenberStatus {
+  id: string;
   name: string;
   imageURL: string;
   basicInfo: string;
--- website/src/app/about/page.tsx
+++ website/src/app/about/page.tsx
@@ -9,10 +9,9 @@ const About = async () => {
     <div className="p-8">
       <h1 className="text-2xl font-bold">About Page</h1>
       <p className="mt-4">This is the About page of the website.</p>
-      {menbers.map((menber, index) => (
-        <div className="border : 1">
+      {menbers.map((menber) => (
+        <div key={menber.id} className="border : 1">
           <ProfileTemplate
-            key={index}
             name={menber.name}
             image={menber.imageURL}
             basicInfo={menber.basicInfo}
