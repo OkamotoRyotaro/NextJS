e734ae33446957426aac4a976113cd5d8b91cb87
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-04-14 09:55:14 +0900

    ヒーローページが自動で遷移するアニメーションを実装

--- website/src/app/top/HeroImage.tsx
+++ website/src/app/top/HeroImage.tsx
@@ -12,12 +12,15 @@ export const HeroImage = () => {
   return (
     <Splide
       options={{
-        rewind: true,
-        gap: '1rem',
-        perPage: 2,
-        type: "loop",
-        autoplay: true,
-        perMove: 1,
+        rewind: true, // スライダーを最後まで行ったら最初に戻る
+        gap: "1rem", // スライド間のギャップ
+        perPage: 1, // 一度に表示するスライド数
+        type: "loop", // 無限ループ
+        autoplay: true, // 自動再生を有効化
+        interval: 1000, // スライド間隔（ミリ秒）
+        perMove: 1, // 一度に移動するスライド数
+        arrows: true, // 左右の矢印を表示
+        pagination: true, // ドットナビゲーションを表示
       }}
       aria-label="お気に入りの写真"
     >
