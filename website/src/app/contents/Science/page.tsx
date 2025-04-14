8ff60695c8e2e0cbd9374b66e684985e9f604aaa
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-04-14 16:26:53 +0900

    記事リスト作成中

--- website/src/app/contents/Science/page.tsx
+++ website/src/app/contents/Science/page.tsx
@@ -1,10 +1,42 @@
+import path from "path";
+import fs from "fs";
 import SciencePageTemplate from "./SciencePageTemplate";
+import Link from "next/link";
 
 export const Science = () => {
+  const filePath = path.join(process.cwd(), "public", "science");
+  const dirList = fs.readdirSync(filePath)
+
+  console.log(dirList)
+
+  const ArticleList = dirList.map((dir, index) => ({
+    id: `article-${index}`,
+    title: dir
+  }))
+
   return (
-    <SciencePageTemplate
-      dirPath={"/science/article0"}
-    />
+    <>
+      {/* <nav>
+        <h2>Index</h2>
+        <ul>
+          {ArticleList.map((article) => (
+            <Link key={article.id} href={""}>
+              {article.title}
+            </Link>
+          ))}
+        </ul>
+      </nav> */}
+      {ArticleList.map((article, index) => (
+        <div
+          key={index}
+          id={`article-${ArticleList.findIndex((elem) => elem.title === article.title)}`}
+        >
+          <SciencePageTemplate
+            dirPath={"/science/article0"}
+          />
+        </div>
+      ))}
+    </>
   )
 }
 export default Science;
