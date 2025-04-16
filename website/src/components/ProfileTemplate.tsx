e6812f31809a2ac1b37b19ab54e9f460c5484656
RYOTARO <okamotoryo@yamaha-robotics.com>
2025-04-16 17:08:44 +0900

    ヘッダーとAbout Us の横スクロールデザインを修正

--- website/src/components/ProfileTemplate.tsx
+++ website/src/components/ProfileTemplate.tsx
@@ -10,20 +10,24 @@ interface ProfileTemplateProps {
 
 export const ProfileTemplate = (props: ProfileTemplateProps) => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <h1 className="text-4xl">{props.name}</h1>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <Image
           src={props.image}
           width={100}
           height={100}
           alt=""
           style={{ width: "30vw", height: "auto" }}
-          className="m-2"
+          className="m-2 w-full md:w-1/3"
         />
-        <pre className="text-2xl whitespace-pre-wrap">{props.basicInfo}</pre>
+        <pre
+          className="whitespace-pre-wrap"
+        >
+          {props.basicInfo}
+        </pre>
       </div>
-      <pre className="text-2xl whitespace-pre-wrap">
+      <pre className="whitespace-pre-wrap">
         {props.history}
       </pre>
     </div>
